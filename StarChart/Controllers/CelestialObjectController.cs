3c8813c64f44f310f55b69fd054a986e4b0cda70
vasilisdmr <vasilisdmr@gmail.com>
2020-11-29 15:31:26 +0200

    Created GetById method.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -13,5 +15,33 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id: int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites = new List<CelestialObject>();
+
+            return Ok(celestialObject);
+        }
+
+        //[HttpGet("{name}")]
+        //public IActionResult GetByName(string name)
+        //{
+        //    var celestialObject = _context.CelestialObjects.Find(name);
+
+        //    if (celestialObject == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+
+        //}
     }
 }
